167ed679c3d0904aab414ac912672d3f60c17b47
Sarwar Bhuiyan <sarwar@elastic.co>
2015-09-22 11:24:55 +0100

    Added the dates, cc, bcc to elasticsearch as well as upgraded elasticsearch version to latest

--- src/main/java/com/bcoe/enronsearch/ElasticSearch.java
+++ src/main/java/com/bcoe/enronsearch/ElasticSearch.java
@@ -3,183 +3,182 @@ package com.bcoe.enronsearch;
 import java.io.IOException;
 
 import org.elasticsearch.client.Client;
-
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
-
 import org.elasticsearch.action.admin.indices.create.*;
 import org.elasticsearch.action.admin.indices.delete.*;
 import org.elasticsearch.action.admin.indices.flush.*;
-
 import org.elasticsearch.action.admin.cluster.health.*;
-
+import org.elasticsearch.common.joda.time.DateTime;
+import org.elasticsearch.common.joda.time.format.DateTimeFormat;
+import org.elasticsearch.common.joda.time.format.DateTimeFormatter;
+import org.elasticsearch.common.joda.time.format.DateTimeFormatterBuilder;
+import org.elasticsearch.common.joda.time.format.DateTimeParser;
+import org.elasticsearch.common.joda.time.format.ISODateTimeFormat;
 import org.elasticsearch.common.settings.*;
-import org.elasticsearch.common.settings.ImmutableSettings;
-
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.index.query.FilterBuilders.*;
 import org.elasticsearch.index.query.QueryBuilders;
-
 import org.elasticsearch.client.transport.*;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 
 /*
-Wrapper for ElasticSearch, performs
-searching and indexing tasks.
-*/
+ Wrapper for ElasticSearch, performs
+ searching and indexing tasks.
+ */
 public class ElasticSearch {
 
-  private static String indexName = "enron-messages";
-  private static String mappingName = "message";
-  private Client client;
+	private static String indexName = "enron-messages";
+	private static String mappingName = "message";
+	private Client client;
+	private static final String DEFAULT_ES_HOST = "localhost";
+	private static final int DEFAULT_ES_PORT = 9300;
 
-  public ElasticSearch() {
-    client = new TransportClient()
-      .addTransportAddress(new InetSocketTransportAddress(
-        System.getenv("ES_HOST"),
-        Integer.parseInt( System.getenv("ES_PORT") )
-      ));
-  }
+	public ElasticSearch() {
+		ImmutableSettings.Builder clientSettingsBuilder = ImmutableSettings
+				.settingsBuilder();
+		String envHost = System.getenv("ES_HOST");
+		String envPort = System.getenv("ES_PORT");
+		String envClusterName = System.getenv("ES_CLUSTER_NAME");
+		if(envClusterName!=null){
+			clientSettingsBuilder.put("cluster.name", envClusterName);
+		}
+		client = new TransportClient(clientSettingsBuilder.build())
+				.addTransportAddress(new InetSocketTransportAddress(
+						envHost != null ? envHost : DEFAULT_ES_HOST,
+						envPort != null ? Integer.parseInt(envPort)
+								: DEFAULT_ES_PORT));
+	}
 
-  public void index() {
-    deleteIndex();
-    createIndex();
-    putMapping();
-  }
+	public void index() {
+		deleteIndex();
+		createIndex();
+		putMapping();
+	}
 
-  private void deleteIndex() {
-    try {
-      client.admin()
-        .indices()
-        .delete(new DeleteIndexRequest(indexName))
-        .actionGet();
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-  }
+	private void deleteIndex() {
+		try {
+			client.admin().indices().delete(new DeleteIndexRequest(indexName))
+					.actionGet();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
-  private void createIndex() {
-    Settings indexSettings = ImmutableSettings.settingsBuilder()
-      .put("number_of_shards", 1)
-      .put("number_of_replicas", 0)
-      .put("analysis.analyzer.default.tokenizer", "uax_url_email")
-      .build();
+	private void createIndex() {
+		Settings indexSettings = ImmutableSettings.settingsBuilder()
+				.put("number_of_shards", 1).put("number_of_replicas", 0)
+				.put("analysis.analyzer.default.tokenizer", "uax_url_email")
+				.build();
 
-    CreateIndexRequestBuilder createIndexBuilder = client.admin()
-      .indices()
-      .prepareCreate(indexName);
+		CreateIndexRequestBuilder createIndexBuilder = client.admin().indices()
+				.prepareCreate(indexName);
 
-    createIndexBuilder.setSettings(indexSettings);
+		createIndexBuilder.setSettings(indexSettings);
 
-    CreateIndexResponse createIndexResponse = createIndexBuilder
-      .execute()
-      .actionGet();
+		CreateIndexResponse createIndexResponse = createIndexBuilder.execute()
+				.actionGet();
 
-    waitForYellow();
-  }
+		waitForYellow();
+	}
 
-  private void waitForYellow() {
-    ClusterHealthRequestBuilder healthRequest = client.admin()
-      .cluster()
-      .prepareHealth();
-    
-    healthRequest.setIndices(indexName);
-    healthRequest.setWaitForYellowStatus();
+	private void waitForYellow() {
+		ClusterHealthRequestBuilder healthRequest = client.admin().cluster()
+				.prepareHealth();
 
-    ClusterHealthResponse healthResponse = healthRequest.execute()
-      .actionGet();
-  }
+		healthRequest.setIndices(indexName);
+		healthRequest.setWaitForYellowStatus();
 
-  private void putMapping() {
-    try {
+		ClusterHealthResponse healthResponse = healthRequest.execute()
+				.actionGet();
+	}
 
-      XContentBuilder mapping = XContentFactory.jsonBuilder()
-         .startObject()
-            .startObject("message")
-              .startObject("properties")
-                .startObject("to")
-                  .field("type", "string")
-                  .field("store", "yes")
-                .endObject()
-                .startObject("from")
-                  .field("type", "string")
-                  .field("store", "yes")
-                .endObject()
-                .startObject("subject")
-                  .field("type", "string")
-                  .field("store", "yes")
-                .endObject()
-                .startObject("body")
-                  .field("type", "string")
-                  .field("store", "yes")
-                .endObject()
-              .endObject()
-            .endObject()
-          .endObject();
+	private void putMapping() {
+		try {
+			
+			XContentBuilder mapping = XContentFactory.jsonBuilder()
+					.startObject().startObject("message")
+						.startObject("properties")
+							.startObject("to")
+								.field("type", "string")
+							.endObject()
+							.startObject("from")
+								.field("type", "string")
+							.endObject()
+							.startObject("subject")
+								.field("type", "string")
+							.endObject()
+							.startObject("body")
+								.field("type", "string")
+							.endObject()
+							.startObject("date")
+								.field("type", "date")
+//								.field("format", "EEE, d MMM yyyy HH:mm:ss Z (z)")
+							.endObject()
+						.endObject()
+					.endObject().endObject();
 
-      client.admin()
-        .indices() 
-        .preparePutMapping(indexName) 
-        .setType(mappingName) 
-        .setSource(mapping) 
-        .execute()
-        .actionGet(); 
+			client.admin().indices().preparePutMapping(indexName)
+					.setType(mappingName).setSource(mapping).execute()
+					.actionGet();
 
-      flush();
-    } catch (IOException e) {
-      System.out.println("Failed to put mapping: " + e);
-    }
-  }
+			flush();
+		} catch (IOException e) {
+			System.out.println("Failed to put mapping: " + e);
+		}
+	}
 
-  private void flush() {
-    client.admin()
-      .indices()
-      .flush(new FlushRequest(indexName))
-      .actionGet();
-  }
+	private void flush() {
+		client.admin().indices().flush(new FlushRequest(indexName)).actionGet();
+	}
 
-  public void indexMessage(Message message) {
-    try {
-      XContentBuilder document = XContentFactory.jsonBuilder()
-        .startObject()
-          .field("to", message.getTo())
-          .field("from", message.getFrom())
-          .field("subject", message.getSubject())
-          .field("body", message.getBody())
-        .endObject();
+	public void indexMessage(Message message) {
+		try {
+			DateTimeParser[] parsers = {
+					DateTimeFormat.forPattern("EEE, d MMM yyyy HH:mm:ss Z (z)").getParser(),
+					DateTimeFormat.forPattern("EEEE, MMMM d, yyyy hh:mm a").getParser()
+			};
+			DateTimeFormatter fmt = ISODateTimeFormat.dateTime();
+			
+			DateTimeFormatter formatter = new DateTimeFormatterBuilder().append(null, parsers).toFormatter();
+			DateTime dateTime = formatter.parseDateTime(message.getDateString());
+			XContentBuilder document = XContentFactory.jsonBuilder()
+					.startObject().field("to", message.getTo())
+					.field("from", message.getFrom())
+					.field("subject", message.getSubject())
+					.field("body", message.getBody())
+					.field("cc", message.getCc())
+					.field("bcc", message.getBcc())
+					.field("date", fmt.print(dateTime))
+					.endObject();
 
-      client.prepareIndex(indexName, mappingName, message.getId())
-        .setSource(document)
-        .execute()
-        .actionGet();
+			client.prepareIndex(indexName, mappingName, message.getId())
+					.setSource(document).execute().actionGet();
 
-    } catch (IOException e) {
-      System.out.println(e);
-    }
-  }
+		} catch (IOException e) {
+			System.out.println(e);
+		} catch(Exception e) {
+			System.out.println("Error parsing date "+message.getDateString()+" for message with subject "+message.getSubject());
+			
+		}
+	}
 
-  public SearchResponse search(String query) {
-    return client.prepareSearch(indexName)
-        .setTypes(mappingName)
-        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-        .setQuery(
-          QueryBuilders.queryString(query)
-            .field("_all")
-            .lenient(true)
-            .autoGeneratePhraseQueries(true)
-            .analyzeWildcard(true)
-            .phraseSlop(10)
-            .lowercaseExpandedTerms(false)
-        )
-        .setFrom(0).setSize(30)
-        .addHighlightedField("to", 0, 0)
-        .addHighlightedField("from", 0, 0)
-        .execute()
-        .actionGet();
-  }
+	public SearchResponse search(String query) {
+		return client
+				.prepareSearch(indexName)
+				.setTypes(mappingName)
+				.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+				.setQuery(
+						QueryBuilders.queryString(query).field("_all")
+								.lenient(true).autoGeneratePhraseQueries(true)
+								.analyzeWildcard(true).phraseSlop(10)
+								.lowercaseExpandedTerms(false)).setFrom(0)
+				.setSize(30).addHighlightedField("to", 0, 0)
+				.addHighlightedField("from", 0, 0).execute().actionGet();
+	}
 
-  public void cleanup() {
-    client.close();
-  }
+	public void cleanup() {
+		client.close();
+	}
 }
